e144196a2c4abefe7796155187983972b334eb70
Uriel3coletivo <urielluizsantos@gmail.com>
2026-06-18 16:41:22 -0300

    Create page.js

---
+++ src/app/src/app/src/app/page.js
@@ -0,0 +1,12 @@
+import Navigation from '@/components/Navigation'
+import Hero from '@/components/Hero'
+
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      {/* Outras seções virão aqui */}
+    </>
+  )
+}

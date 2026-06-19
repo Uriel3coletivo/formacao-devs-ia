09f6222fef748be021bb026994262bf88313ae72
Uriel3coletivo <urielluizsantos@gmail.com>
2026-06-19 13:44:20 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Navigation from '../components/Navigation'
 import Hero from '../components/Hero'
 import Oportunidade from '../components/Oportunidade'
 import Imagine from '../components/Imagine'
+import Diferenciais from '../components/Diferenciais'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Oportunidade />
       <Imagine />
+      <Diferenciais />
     </main>
   )
 }

ed2bf6bb82e296085909c51b520ea750b80237d1
Uriel3coletivo <urielluizsantos@gmail.com>
2026-06-22 15:38:32 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Forçando o celular a baixar as fontes antes de tudo */}
+        <link rel="preload" href="/fonts/AgibankSans-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/AgibankSans-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/AgibankSans-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/AgibankSans-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+      </head>
       <body>{children}</body>
     </html>
   )
